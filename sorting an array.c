10761f621cd98fd5eeabb85d7b0e1d8e6d784488
waytoshankaranand <32411198+waytoshankaranand@users.noreply.github.com>
2022-04-07 20:03:27 +0530

    Update sorting an array.c

--- sorting an array.c
+++ sorting an array.c
@@ -48,6 +48,26 @@ int sort(int *arr1, int *s)
     }
 }
 
+
+void sort1(int arr[], int n)
+    {
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=0;j<n-1;j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    int temp;
+                    temp= arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+
+
+
 int showarr(int *arr1, int *s)
 {
     int i;
